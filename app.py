f02efdf252c26d8cef411e5daa59e68fa721dfef
Pierre Locus <lop@nalios.be>
2023-11-24 23:00:46 +0100

    [ADD] Skelton of app.py + templates

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/heartbeat')
+def heartbeat():
+    return
+
+@app.route('/shooter')
+def game():
+    return render_template('shooter.html')
+
+@app.route('deskclean')
+def deskclean():
+    return render_template('deskclean.html')
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('leaderboard.html')
+
+@app.route('/leaderboardpc')
+def leaderboardpc():
+    return render_template('leaderboardpc.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
