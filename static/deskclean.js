ea594199992ea3a4686d7dcc9312169de3e2e362
Pierre Locus <lop@nalios.be>
2023-11-25 22:27:11 +0100

    [FIx]

--- static/deskclean.js
+++ static/deskclean.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const elements = [{ "Name": "mouse", "positionxgood": "309px", "positionygood": "540px", "positionxbad": "200px", "positionybad": "400px" },
-    { "Name": "keys", "positionxgood": "320px", "positionygood": "130px", "positionxbad": "300px", "positionybad": "100px" },
-    { "Name": "wallet", "positionxgood": "295px", "positionygood": "600px", "positionxbad": "50px", "positionybad": "500px" },
-    { "Name": "screen", "positionxgood": "194px", "positionygood": "384px", "positionxbad": "150px", "positionybad": "200px" },
-    { "Name": "keyboard", "positionxgood": "293px", "positionygood": "384px", "positionxbad": "300px", "positionybad": "300px" },
-    { "Name": "mug", "positionxgood": "270px", "positionygood": "200px", "positionxbad": "150px", "positionybad": "500px" },
-    { "Name": "casse_tete", "positionxgood": "305px", "positionygood": "740px", "positionxbad": "150px", "positionybad": "600px" },
-    { "Name": "chien", "positionxgood": "257px", "positionygood": "554px", "positionxbad": "250px", "positionybad": "300px" },
+    { "Name": "keys", "positionxgood": "320px", "positionygood": "180px", "positionxbad": "300px", "positionybad": "100px" },
+    { "Name": "wallet", "positionxgood": "295px", "positionygood": "650px", "positionxbad": "50px", "positionybad": "500px" },
+    { "Name": "screen", "positionxgood": "194px", "positionygood": "394px", "positionxbad": "150px", "positionybad": "200px" },
+    { "Name": "keyboard", "positionxgood": "293px", "positionygood": "394px", "positionxbad": "300px", "positionybad": "300px" },
+    { "Name": "mug", "positionxgood": "280px", "positionygood": "230px", "positionxbad": "150px", "positionybad": "500px" },
+    { "Name": "casse_tete", "positionxgood": "305px", "positionygood": "750px", "positionxbad": "150px", "positionybad": "600px" },
+    { "Name": "chien", "positionxgood": "257px", "positionygood": "564px", "positionxbad": "250px", "positionybad": "300px" },
     ];
 
     setInterval(() => {
