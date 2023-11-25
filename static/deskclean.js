1e5113b119bada5ef6ed12b256b0e3a9c138cef7
lde-nalios <lde@nalios.be>
2023-11-25 17:10:23 +0100

    [deskclean] change design and add max lost of point for 1 element

--- static/deskclean.js
+++ static/deskclean.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const elements = [{ "Name": "mouse", "positionxgood": "10px", "positionygood": "100px", "positionxbad": "200px", "positionybad": "400px" },
-    { "Name": "apple", "positionxgood": "100px", "positionygood": "400px", "positionxbad": "100px", "positionybad": "100px" },
-    { "Name": "coffee", "positionxgood": "150px", "positionygood": "150px", "positionxbad": "50px", "positionybad": "100px" },
-    { "Name": "keys", "positionxgood": "0px", "positionygood": "400px", "positionxbad": "300px", "positionybad": "300px" },
-    { "Name": "wallet", "positionxgood": "200px", "positionygood": "600px", "positionxbad": "0px", "positionybad": "500px" },
-    { "Name": "keyboard", "positionxgood": "50px", "positionygood": "500px", "positionxbad": "150px", "positionybad": "300px" }];
+    const elements = [{ "Name": "mouse", "positionxgood": "309px", "positionygood": "540px", "positionxbad": "200px", "positionybad": "400px" },
+    { "Name": "keys", "positionxgood": "320px", "positionygood": "130px", "positionxbad": "300px", "positionybad": "100px" },
+    { "Name": "wallet", "positionxgood": "295px", "positionygood": "600px", "positionxbad": "50px", "positionybad": "500px" },
+    { "Name": "screen", "positionxgood": "194px", "positionygood": "384px", "positionxbad": "150px", "positionybad": "200px" },
+    { "Name": "keyboard", "positionxgood": "293px", "positionygood": "384px", "positionxbad": "300px", "positionybad": "300px" },
+    { "Name": "mug", "positionxgood": "270px", "positionygood": "200px", "positionxbad": "150px", "positionybad": "500px" },
+    { "Name": "casse_tete", "positionxgood": "305px", "positionygood": "740px", "positionxbad": "150px", "positionybad": "600px" },
+    { "Name": "chien", "positionxgood": "257px", "positionygood": "554px", "positionxbad": "250px", "positionybad": "300px" },
+    ];
+
     setInterval(() => {
         $.ajax({
             url: '/heartbeat',
@@ -27,41 +31,45 @@ document.addEventListener("DOMContentLoaded", function () {
         element.style.top = elementId.positionygood;
     }
     elements.forEach(placeImage);
+});
 
 
 
-    function addTouchMoveListener(elementId) {
-        const element = document.getElementById(elementId.Name);
+function addTouchMoveListener(elementId) {
+    const element = document.getElementById(elementId.Name);
 
-        element.addEventListener('touchmove', function (e) {
-            const touchLocation = e.targetTouches[0];
-            element.style.left = touchLocation.pageX + 'px';
-            element.style.top = touchLocation.pageY + 'px';
-        });
+    element.addEventListener('touchmove', function (e) {
+        const touchLocation = e.targetTouches[0];
+        element.style.left = touchLocation.pageX + 'px';
+        element.style.top = touchLocation.pageY + 'px';
+    });
+};
+
+function shakePosition(elementId) {
+    var element = document.getElementById(elementId.Name);   
+    element.style.left = elementId.positionxbad;
+    element.style.top = elementId.positionybad;
+};
+
+function parsePosition(position) {
+    return parseInt(position.slice(0, -2));
+};
+
+var points = 1000;
+function countPoints(elementId) {
+    var element = document.getElementById(elementId.Name);
+    xdistance = Math.abs(parsePosition(element.style.left) - parsePosition(elementId.positionxgood));
+    ydistance = Math.abs(parsePosition(element.style.top) - parsePosition(elementId.positionygood));
+    final_distance = Math.pow(Math.pow(xdistance, 2) + Math.pow(ydistance, 2), 0.5);
+    if (final_distance > 125) {
+        final_distance = 125;
+    }
+    points = points - final_distance;
+    if (points < 0){
+        points = 0;
     }
 
-    function shakePosition(elementId) {
-        var element = document.getElementById(elementId.Name);
 
-        element.style.left = elementId.positionxbad;
-        element.style.top = elementId.positionybad;
-    };
-
-    function parsePosition(position) {
-        return parseInt(position.slice(0, -2));
-    };
-
-    var points = 1000;
-    function countPoints(elementId) {
-        var element = document.getElementById(elementId.Name);
-        xdistance = Math.abs(parsePosition(element.style.left) - parsePosition(elementId.positionxgood));
-        ydistance = Math.abs(parsePosition(element.style.top) - parsePosition(elementId.positionygood));
-        final_distance = Math.pow(Math.pow(xdistance, 2) + Math.pow(ydistance, 2), 0.5);
-        points = points - final_distance;
-        if (points < 0) {
-            points = 0;
-        }
-    };
 
     function scoreDisplay(points) {
         const pointsDiv = document.getElementById('points');
@@ -129,4 +137,4 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 1000);
     }, 25000);
 
-});
+};
